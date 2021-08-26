833f9de5691906d5a4910de337a2e970c909902f
wpbindt <wesselbindt@gmail.com>
2021-08-26 09:07:00 +0200

    with-type-hints-no-stubs

--- api_client.py
+++ api_client.py
@@ -1,18 +1,22 @@
+import requests
+
+
 class InvitationAPI:
     base_url = 'your-wedding.com/wessel'
 
-    def add(self, name):
+    def add(self, name: str) -> None:
         self._send_request(f'/add/{name}')
 
-    def remove(self, name):
+    def remove(self, name: str) -> None:
         self._send_request(f'/delete/{name}')
 
-    def _send_request(self, path):
+    def _send_request(self, path: str) -> requests.Response:
         # some stuff actually talking to the API
         print(f'sending request with path {self.base_url + path}')
+        return requests.Response()
 
 
-def main(api_client):
+def main(api_client: InvitationAPI) -> None:
     api_client.add('Mary Antoinette')
     api_client.add('North West')
     api_client.remove('Salieri')
